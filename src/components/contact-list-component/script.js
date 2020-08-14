33a8f4b73b71b6a3511a85b5ece72bce3f0c503e
sas <sas@attrax.digital>
2020-08-14 11:43:54 +0700

    added contact-list-component

---
+++ src/components/contact-list-component/script.js
@@ -0,0 +1,19 @@
+export default {
+    name: 'contact-list-component',
+    props: {
+
+    },
+    data: function () {
+        return {
+
+        }
+    },
+    created() {
+    },
+
+    methods: {
+        inputting(event) {
+            this.$emit('typing', event.target.value)
+        }
+    }
+}
