d7206145f1d80942bcde6188f3ab5b7296a3b00c
sas <sas@attrax.digital>
2020-08-15 14:31:11 +0700

    rename " home-page"

--- src/pages/home-page/script.js
+++ src/pages/home-page/script.js
@@ -2,7 +2,7 @@ import homePage from "../home-page/home-page";
 import mainContentComponent from "../../components/main-content-component/main-content-component";
 
 export default {
-    name: ' home-page',
+    name: 'home-page',
     props: {
 
     },
